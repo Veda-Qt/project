e51705192cb85d619f7cb8e5e270d7877d3bcd3f
reclred <paulhong306@gmail.com>
2026-04-20 17:43:00 +0900

    CLEANUP: modify LineEdit text

--- build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_project_FILE C:/QtSource/Qt Projects/project/build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/project.exe)
-set(__QT_DEPLOY_TARGET_project_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_project_RUNTIME_DLLS C:/Qt/6.11.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.11.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.11.0/mingw_64/bin/Qt6Core.dll)
