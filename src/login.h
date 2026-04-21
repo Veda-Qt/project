6500f945289b2b2f4ff4caa93f0a13aecfb6554c
reclred <paulhong306@gmail.com>
2026-04-21 10:23:22 +0900

    CLEARUP: wrote basic structure of login.h

--- src/login.h
+++ src/login.h
@@ -1,4 +1,23 @@
 #ifndef LOGIN_H
 #define LOGIN_H
 
+#include <QWidget>
+
+QT_BEGIN_NAMESPACE
+namespace Ui{
+class Login;
+}
+QT_END_NAMESPACE
+
+class Login: public QWidget
+{
+    Q_OBJECT
+public:
+
+private:
+
+private slots:
+
+};
+
 #endif // LOGIN_H
