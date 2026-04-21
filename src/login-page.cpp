e2b0b5f6a0e3209c1719b9255914e04d858630ed
reclred <paulhong306@gmail.com>
2026-04-21 10:30:35 +0900

    CLEANUP: modified file name of head file & source file

--- src/login-page.cpp
+++ src/login-page.cpp
@@ -1,1 +1,14 @@
-#include "login.h"
+#include "login-page.h"
+#include "./ui_login-page.h"
+
+LoginPage::LoginPage(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::LoginPage)
+{
+    ui->setupUi(this);
+}
+
+LoginPage::~LoginPage()
+{
+    delete ui;
+}
--- src/login-page.h
+++ src/login-page.h
@@ -1,23 +1,24 @@
-#ifndef LOGIN_H
-#define LOGIN_H
+#ifndef LOGINPAGE_H
+#define LOGINPAGE_H
 
 #include <QWidget>
 
 QT_BEGIN_NAMESPACE
 namespace Ui{
-class Login;
+class LoginPage;
 }
 QT_END_NAMESPACE
 
-class Login: public QWidget
+class LoginPage: public QWidget
 {
     Q_OBJECT
 public:
-
+    explicit LoginPage(QWidget *parent = nullptr);
+    ~LoginPage() override;
 private:
-
+    Ui::LoginPage *ui;
 private slots:
 
 };
 
-#endif // LOGIN_H
+#endif // LOGINPAGE_H
